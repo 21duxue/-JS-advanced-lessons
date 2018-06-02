993ae7c454a71f19a8f8395bb308cde963f6fcd2
21duxue <1479605026@qq.com>
2018-06-02 15:03:06 +0800

    6.1

---
+++ 课程练习/LS26/JS/demo0.js
@@ -0,0 +1,60 @@
+// 案例二 异步执行的回调(通过定时器模拟)
+var LTimeOperation = function(taskID){
+    var id = taskID;
+    //思考什么时候将方法定义在构造函数内，什么时候定义在构造函数的prototype属性上
+    this.go = function(callback){
+        console.log('Start LTimeOperation #'+id);
+        var delay = parseInt((Math.random() * 10000000) % 5000);
+        setTimeout(function(){
+            console.log('task #'+id+' cost '+delay+' ms.');
+            callback();
+        },delay);
+    }
+};
+function f2(){
+    console.log('this is f2, i am callback!\n');
+}
+for(var i = 0;i<5;i++){
+    var task = new LTimeOperation(i);
+    task.go(f2);
+}
+
+
+
+//发布订阅 实例/////////////////////
+var Subject = function(){
+    var _observer = [];
+    this.attach = function(observer){
+        _observer.push(observer);
+    };
+    this.detach = function(){
+        _observer.pop();
+    };
+    this.notify = function(msg){
+        for(var i=0;i<_observer.length;i++){
+            _observer[i].update(msg);
+        }
+    };
+    this.print = function(){
+        console.log(_observer.length);
+        console.log(_observer);
+    };
+};
+var Observer = function(name){
+    this.update = function(msg){
+        console.log('i am '+name+',and i get the message: '+msg);
+    };
+};
+var sub = new Subject()
+sub.attach(new Observer('a'));
+sub.attach(new Observer('b'));
+sub.notify('hello');
+//sub.print();
+
+setTimeout(function(){
+    var c = new Observer('c');
+    sub.detach();
+    sub.attach(c);
+    sub.notify('world');
+    //sub.print();
+},5000);
